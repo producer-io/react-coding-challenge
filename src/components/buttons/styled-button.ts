93828a4833724f60fe57669f1c7920e9690373db
Mohamed Hisham Moubarak <mohamed.hisham00@gmail.com>
2023-01-14 17:12:35 +0200

    [refactor] Export common button styles to StyledButton

---
+++ src/components/buttons/styled-button.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+border-radius: .8rem;
+padding: .6rem 1.2rem;
+font-size: 1rem;
+font-weight: 500;
+font-family: inherit;
+
+&:hover {
+  cursor: pointer;
+}
+`
